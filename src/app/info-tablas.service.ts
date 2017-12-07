e49885d3372ac1df82663f721c7f20539fa10ddb
daniel2389 <prjchec.dcardona@umanizales.edu.co>
2017-12-06 20:18:40 -0500

    Ordenamiento de tablas segun su valor de frecuencia

--- src/app/info-tablas.service.ts
+++ src/app/info-tablas.service.ts
@@ -3,141 +3,141 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class InfoTablasService {
 
-  public tablaChats:any = {
+  public tablaChats: any = {
     headers: ['Palabra', 'Frecuencia'],
     rows: [
+      ['GRACIAS', '5772'],
       ['SI', '4365'],
       ['FACTURA', '3412'],
-      ['GRACIAS', '5772'],
-      ['SEÑORA', '1961'],
       ['MOMENTO', '2579'],
       ['CHEC', '2557'],
-      ['CHEC_CHAT', '1515'],
-      ['FAVOR', '1947'],
       ['NOMBRE', '2389'],
+      ['SEÑORA', '1961'],
+      ['FAVOR', '1947'],
+      ['CHEC CHAT', '1515'],
       ['CUENTA', '1437']
     ]
   }
-  public tablaEncuestas:any = {
+  public tablaEncuestas: any = {
     headers: ['Palabra', 'Frecuencia'],
     rows: [
-      ['ORIENTACIÓN', '339'],
-      ['USUARIO', '636'],
       ['INDICA', '736'],
-      ['FACTURA', '420'],
+      ['USUARIO', '636'],
       ['SERVICIO', '550'],
-      ['ENERGÍA', '318'],
-      ['CONOCIMIENTO', '300'],
+      ['FACTURA', '420'],
       ['INFORMACIÓN', '369'],
+      ['ORIENTACIÓN', '339'],
+      ['ENERGÍA', '318'],
       ['RECIBIDO', '316'],
+      ['CONOCIMIENTO', '300'],
       ['CHEC', '259']
     ]
   }
-  public tablaLlamadas:any = {
+  public tablaLlamadas: any = {
     headers: ['Palabra', 'Frecuencia'],
     rows: [
-      ['NUMERO', '1826'],
-      ['BUENO', '989'],
-      ['DIA', '705'],
+      ['NÚMERO', '1826'],
       ['AQUÍ', '1335'],
       ['NOMBRE', '1113'],
-      ['MOMENTO', '719'],
       ['SEIS', '1090'],
+      ['BUENO', '989'],
       ['FAVOR', '865'],
+      ['MOMENTO', '719'],
+      ['DIA', '705'],
+      ['SERVICIO', '600'],
       ['INFORMACIÓN', '366'],
       ['DIRECCIÓN', '164'],
-      ['SERVICIO', '600']
     ]
   }
 
-  public tablaOficios:any = {
+  public tablaOficios: any = {
     headers: ['Palabra', 'Frecuencia'],
     rows: [
       ['MANIZALES', '1804'],
-      ['CONSUMO', '705'],
       ['CHEC', '1649'],
       ['CALDAS', '1143'],
+      ['VALOR', '886'],
       ['SERVICIO', '828'],
       ['EMPRESA', '732'],
-      ['DERECHO', '604'],
-      ['VALOR', '886'],
+      ['CONSUMO', '705'],
       ['CUENTA', '683'],
+      ['DERECHO', '604'],
       ['NÚMERO', '473'],
       ['CONTRATO', '396']
     ]
   }
 
-  public tablaPQR:any = {
+  public tablaPQR: any = {
     headers: ['Palabra', 'Frecuencia'],
     rows: [
       ['PAGO', '15330'],
       ['SOLICITUD', '12816'],
       ['FACTURA', '10464'],
       ['EMPRESA', '9227'],
+      ['CAMBIO', '6265'],
       ['CONSUMO', '5158'],
       ['ENTREGA', '4771'],
+      ['PETICIÓN', '4273'],
       ['USUARIO', '3834'],
       ['SUSCRIPTOR', '3588'],
-      ['CAMBIO', '6265'],
-      ['PETICIÓN', '4273']
     ]
   }
 
-  public tablaChatsSentimiento:any = {
+  public tablaChatsSentimiento: any = {
     headers: ['Sentimiento', 'Frecuencia'],
     rows: [
-      ['Muy Negativo', '31'],
-      ['Muy Positivo', '1647'],
-      ['Negativo', '1876'],
+      ['Positivo', '7879'],
       ['Neutro', '1961'],
-      ['Positivo', '7879']
+      ['Negativo', '1876'],
+      ['Muy Positivo', '1647'],
+      ['Muy Negativo', '31'],
     ]
   }
 
-  public tablaEncuestasSentimiento:any = {
+  public tablaEncuestasSentimiento: any = {
     headers: ['Sentimiento', 'Frecuencia'],
     rows: [
-      ['Muy Negativo', '7'],
-      ['Muy Positivo', '58'],
-      ['Negativo', '929'],
       ['Neutro', '4338'],
-      ['Positivo', '651']
+      ['Negativo', '929'],
+      ['Positivo', '651'],
+      ['Muy Positivo', '58'],
+      ['Muy Negativo', '7'],
     ]
   }
 
-  public tablaLlamadasSentimiento:any = {
+  public tablaLlamadasSentimiento: any = {
     headers: ['Sentimiento', 'Frecuencia'],
     rows: [
-      ['Muy Negativo', '58'],
       ['Muy Positivo', '558'],
+      ['Positivo', '84'],
       ['Negativo', '60'],
+      ['Muy Negativo', '58'],
       ['Neutro', '15'],
-      ['Positivo', '84']
     ]
   }
 
-  public tablaOficiosSentimiento:any = {
+  public tablaOficiosSentimiento: any = {
     headers: ['Sentimiento', 'Frecuencia'],
     rows: [
-      ['Muy Negativo', '12'],
-      ['Muy Positivo', '339'],
-      ['Negativo', '893'],
       ['Neutro', '130400'],
-      ['Positivo', '2334']
+      ['Positivo', '2334'],
+      ['Negativo', '893'],
+      ['Muy Positivo', '339'],
+      ['Muy Negativo', '12'],
     ]
   }
 
-  public tablaPQRSentimiento:any = {
+  public tablaPQRSentimiento: any = {
     headers: ['Sentimiento', 'Frecuencia'],
     rows: [
-      ['Muy Negativo', '55'],
-      ['Muy Positivo', '1281'],
-      ['Negativo', '8572'],
       ['Neutro', '64814'],
-      ['Positivo', '5236']
+      ['Negativo', '8572'],
+      ['Positivo', '5236'],
+      ['Muy Positivo', '1281'],
+      ['Muy Negativo', '55'],
     ]
   }
-  
+
 
   constructor() { }
 
